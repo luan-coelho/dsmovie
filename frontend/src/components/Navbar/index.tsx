d5d243fc777a2f96523a187c1738d6270be493d8
luanbunny <luancoelho.dev@gmail.com>
2022-01-17 02:34:12 -0300

    Github profile name navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,10 +7,10 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/luanbunny" target="_blank" rel="noreferrer">
+          <a href="https://github.com/luan-coelho" target="_blank" rel="noreferrer">
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/luanbunny</p>
+              <p className="dsmovie-contact-link">/luan-coelho</p>
             </div>
           </a>
         </div>
